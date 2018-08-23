7c9b591e6fa0c1bdeca76944d2f3ac9deaee9d84
Konark Jha <konarkjha@gmail.com>
2018-08-23 09:38:25 +0530

    adding arrays list

---
+++ TypeScript/array.js
@@ -0,0 +1,6 @@
+var list = [1, 2, 3];
+console.log(list[0], list[2] + " printing array ");
+var x;
+x = ["score", 10];
+console.log(x + " print x only ");
+console.log(x[0].substr(2) + " printing part of string ");
---
+++ TypeScript/array.ts
@@ -0,0 +1,11 @@
+let list: number[] = [1, 2, 3];
+
+console.log(list[0], list[2] + " printing array ");
+
+let x: [string, number];
+
+x = ["score", 10];
+
+console.log(x + " print x only ");
+
+console.log(x[0].substr(2) + " printing part of string ");
